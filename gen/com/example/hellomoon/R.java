e513d12266d95b21aac292803ae9b4198f3745be
Kevin Shen <kevinshen34@gmail.com>
2014-08-21 18:41:24 -0700

    audio player created

--- gen/com/example/hellomoon/R.java
+++ gen/com/example/hellomoon/R.java
@@ -25,10 +25,15 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090000;
+        public static final int action_settings=0x7f090004;
+        public static final int helloMoonFragment=0x7f090000;
+        public static final int hellomoon_pauseButton=0x7f090002;
+        public static final int hellomoon_playButton=0x7f090001;
+        public static final int hellomoon_stopButton=0x7f090003;
     }
     public static final class layout {
         public static final int activity_hello_moon=0x7f030000;
+        public static final int fragment_hello_moon=0x7f030001;
     }
     public static final class menu {
         public static final int hello_moon=0x7f080000;
@@ -41,6 +46,10 @@ public final class R {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
         public static final int hello_world=0x7f060002;
+        public static final int hellomoon_description=0x7f060005;
+        public static final int hellomoon_pause=0x7f060006;
+        public static final int hellomoon_play=0x7f060003;
+        public static final int hellomoon_stop=0x7f060004;
     }
     public static final class style {
         /** 
@@ -57,12 +66,6 @@ public final class R {
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
     
  API 11 theme customizations can go here. 
-
-        Base application theme for API 14+. This theme completely replaces
-        AppBaseTheme from BOTH res/values/styles.xml and
-        res/values-v11/styles.xml on API 14+ devices.
-    
- API 14 theme customizations can go here. 
          */
         public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
--- src/com/example/hellomoon/HelloMoonActivity.java
+++ src/com/example/hellomoon/HelloMoonActivity.java
@@ -1,22 +1,14 @@
 package com.example.hellomoon;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.app.FragmentActivity;
 
-public class HelloMoonActivity extends Activity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_hello_moon);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.hello_moon, menu);
-		return true;
-	}
-
+public class HelloMoonActivity extends FragmentActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_moon);
+    }
 }
+
